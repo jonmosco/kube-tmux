79a4e8c9420e8778129e4a0a95fa2805ff1c82df
Jonathan Mosco <jonny.mosco@gmail.com>
2025-06-23 13:41:39 -0400

    update README

--- README.md
+++ README.md
@@ -14,46 +14,98 @@ it to the tmux status line.
 This is the first iteration of this plugin, so the updates will be very
 frequent.  Track this repo for changes, and keep in mind some of them
 might cause issues, although I will try to keep them at a minimum, and provide
-fixes as soon as possible. Please be patient! 
+fixes as soon as possible. Please be patient!
 
 If you have any bug reports, please feel free to submit a PR, or a bug report.
 
 ## Installing
 
+### Manual
+
 Clone this repository to your `$HOME/.tmux` directory, and add the following line to your `~/.tmux.conf`:
 
-```bash
+```sh
 set -g status-right "#(/bin/bash $HOME/.tmux/kube-tmux/kube.tmux 250 red cyan)"
 ```
 
 250 is the color selection for the default foreground, red for the context,
 and cyan for the namespace.
 
+### TPM (Recommended)
+
+```sh
+set -g @plugin 'tmux-plugins/tpm' # mandatory
+set -g '@jonmosco/kube-tmux'
+
+set -g @kube-tmux '250' 'red' 'cyan'
+```
+
 ## Requirements
 
 * tmux
-* kubectl
+* kubectl and/or oc
 
 ## Plugin Structure
 
 The default plugin layout is:
 
-```
+```sh
 <symbol> <cluster>:<namespace>
 ```
 
 If the current-context is not set, kube-tmux will return the following:
 
-```
+```sh
 <symbol> N/A:N/A
 ```
 
 ## Customization
+
 The default color for the context are red, and cyan for the namespace
 Colors for the default text, context, and namespace can be changed:
 
-```bash
+```sh
 #(/bin/bash $HOME/.tmux/kube-tmux/kube.tmux text context namespace)
 ```
 
-## Contributors
+## Customize display of cluster name and namespace
+
+You can change how the cluster name and namespace are displayed using the
+`KUBE_TMUX_CLUSTER_FUNCTION` and `KUBE_TMUX_NAMESPACE_FUNCTION` variables
+respectively.
+
+For the following examples let's assume the following:
+
+cluster name: `sandbox.k8s.example.com`
+namespace: `alpha`
+
+If you're using domain style cluster names, your prompt will get quite long
+very quickly. Let's say you only want to display the first portion of the
+cluster name (`sandbox`), you could do that by adding the following:
+
+```sh
+function get_cluster_short() {
+  echo "$1" | cut -d . -f1
+}
+
+KUBE_TMUX_CLUSTER_FUNCTION=get_cluster_short
+```
+
+The same pattern can be followed to customize the display of the namespace.
+Let's say you would prefer the namespace to be displayed in all uppercase
+(`ALPHA`), here's one way you could do that:
+
+```sh
+function get_namespace_upper() {
+    echo "$1" | tr '[:lower:]' '[:upper:]'
+}
+
+export KUBE_TMUX_NAMESPACE_FUNCTION=get_namespace_upper
+```
+
+In both cases, the variable is set to the name of the function, and you must have defined the function in your shell configuration before kube_ps1 is called. The function must accept a single parameter and echo out the final value.
+
+| Variable | Default | Meaning |
+| :------- | :-----: | ------- |
+| `KUBE_TMUX_CLUSTER_FUNCTION` | No default, must be user supplied | Function to customize how cluster is displayed |
+| `KUBE_TMUX_NAMESPACE_FUNCTION` | No default, must be user supplied | Function to customize how namespace is displayed |
---
+++ scripts/kube-tmux.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+
+# Kubernetes status line for tmux
+# Displays current context and namespace
+
+# Copyright 2025 Jon Mosco
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# shellcheck disable=SC2034
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Default values for the plugin
+KUBE_TMUX_BINARY="${KUBE_TMUX_BINARY:-kubectl}"
+KUBE_TMUX_SYMBOL_ENABLE="${KUBE_TMUX_SYMBOL_ENABLE:-true}"
+KUBE_TMUX_SYMBOL_DEFAULT="${KUBE_TMUX_SYMBOL_DEFAULT:-\u2388 }"
+KUBE_TMUX_SYMBOL_USE_IMG="${KUBE_TMUX_SYMBOL_USE_IMG:-false}"
+KUBE_TMUX_CONTEXT_ENABLE="${KUBE_TMUX_CONTEXT_ENABLE:-true}"
+KUBE_TMUX_NAMESPACE_ENABLE="${KUBE_TMUX_NAMESPACE_ENABLE:-true}"
+KUBE_TMUX_DIVIDER="${KUBE_TMUX_DIVIDER-:}"
+KUBE_TMUX_SYMBOL_COLOR="${KUBE_TMUX_SYMBOL_COLOR-blue}"
+KUBE_TMUX_CTX_COLOR="${KUBE_TMUX_CTX_COLOR-red}"
+KUBE_TMUX_NS_COLOR="${KUBE_TMUX_NS_COLOR-cyan}"
+KUBE_TMUX_KUBECONFIG_CACHE="${KUBECONFIG}"
+KUBE_TMUX_LAST_TIME=0
+
+_kube_tmux_binary_check() {
+  command -v "$1" >/dev/null
+}
+
+# Determine our shell
+_kube_tmux_shell_type() {
+  local _KUBE_TMUX_SHELL_TYPE
+
+  if [ "${ZSH_VERSION-}" ]; then
+    _KUBE_TMUX_SHELL_TYPE="zsh"
+  elif [ "${BASH_VERSION-}" ]; then
+    _KUBE_TMUX_SHELL_TYPE="bash"
+  fi
+  echo $_KUBE_TMUX_SHELL_TYPE
+}
+
+_kube_tmux_symbol() {
+  if ((BASH_VERSINFO[0] >= 4)) && [[ $'\u2388 ' != "\\u2388 " ]]; then
+    KUBE_TMUX_SYMBOL=$'\u2388 '
+    KUBE_TMUX_SYMBOL_IMG=$'\u2638 '
+  else
+    KUBE_TMUX_SYMBOL=$'\xE2\x8E\x88 '
+    KUBE_TMUX_SYMBOL_IMG=$'\xE2\x98\xB8 '
+  fi
+
+  if [[ "${KUBE_TMUX_SYMBOL_USE_IMG}" == true ]]; then
+    KUBE_TMUX_SYMBOL="${KUBE_TMUX_SYMBOL_IMG}"
+  fi
+
+  echo "${KUBE_TMUX_SYMBOL}"
+}
+
+_kube_tmux_split() {
+  type setopt >/dev/null 2>&1 && setopt SH_WORD_SPLIT
+  local IFS=$1
+  echo $2
+}
+
+_kube_tmux_file_newer_than() {
+  local mtime
+  local file=$1
+  local check_time=$2
+
+  if [[ "$(_kube_tmux_shell_type)" == "zsh" ]]; then
+    # Use zstat '-F %s.%s' to make it compatible with low zsh version (eg: 5.0.2)
+    mtime=$(zstat +mtime -F %s.%s "${file}")
+  elif stat -c "%s" /dev/null &> /dev/null; then
+    # GNU stat
+    mtime=$(stat -L -c %Y "${file}")
+  else
+    # BSD stat
+    mtime=$(stat -L -f %m "$file")
+  fi
+
+  [[ "${mtime}" -gt "${check_time}" ]]
+}
+
+_kube_tmux_update_cache() {
+  if ! _kube_tmux_binary_check "${KUBE_TMUX_BINARY}"; then
+    # No ability to fetch context/namespace; display N/A.
+    KUBE_TMUX_CONTEXT="BINARY-N/A"
+    KUBE_TMUX_NAMESPACE="N/A"
+    return
+  fi
+
+  if [[ "${KUBECONFIG}" != "${KUBE_TMUX_KUBECONFIG_CACHE}" ]]; then
+    # User changed KUBECONFIG; unconditionally refetch.
+    KUBE_TMUX_KUBECONFIG_CACHE=${KUBECONFIG}
+    _kube_tmux_get_context_ns
+    return
+  fi
+
+  # kubectl will read the environment variable $KUBECONFIG
+  # otherwise set it to ~/.kube/config
+  local conf
+  for conf in $(_kube_tmux_split : "${KUBECONFIG:-${HOME}/.kube/config}"); do
+    [[ -r "${conf}" ]] || continue
+    if _kube_tmux_file_newer_than "${conf}" "${KUBE_TMUX_LAST_TIME}"; then
+      _kube_tmux_get_context_ns
+      return
+    fi
+  done
+}
+
+_kube_tmux_get_context() {
+  if [[ "${KUBE_TMUX_CONTEXT_ENABLE}" == true ]]; then
+    KUBE_TMUX_CONTEXT="$(${KUBE_TMUX_BINARY} config current-context 2>/dev/null)"
+    # Set namespace to 'N/A' if it is not defined
+    KUBE_TMUX_CONTEXT="${KUBE_TMUX_CONTEXT:-N/A}"
+
+    if [[ -n "${KUBE_TMUX_CLUSTER_FUNCTION}" ]]; then
+      KUBE_TMUX_CONTEXT="$("${KUBE_TMUX_CLUSTER_FUNCTION}" "${KUBE_TMUX_CONTEXT}")"
+    fi
+  fi
+}
+
+_kube_tmux_get_ns() {
+  if [[ "${KUBE_TMUX_NAMESPACE_ENABLE}" == true ]]; then
+    KUBE_TMUX_NAMESPACE="$(${KUBE_TMUX_BINARY} config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)"
+    KUBE_TMUX_NAMESPACE="${KUBE_TMUX_NAMESPACE:-N/A}"
+
+    if [[ -n "${KUBE_TMUX_NAMESPACE_FUNCTION}" ]]; then
+        KUBE_TMUX_NAMESPACE="$("${KUBE_TMUX_NAMESPACE_FUNCTION}" "${KUBE_TMUX_NAMESPACE}")"
+    fi
+  fi
+}
+
+_kube_tmux_get_context_ns() {
+  # Set the command time
+  if [[ "${KUBE_TMUX_SHELL}" == "bash" ]]; then
+    if ((BASH_VERSINFO[0] >= 4)); then
+      KUBE_TMUX_LAST_TIME=$(printf '%(%s)T')
+    else
+      KUBE_TMUX_LAST_TIME=$(date +%s)
+    fi
+  fi
+
+  if [[ "${KUBE_TMUX_NAMESPACE_ENABLE}" == true ]]; then
+    KUBE_TMUX_NAMESPACE="$(${KUBE_TMUX_BINARY} config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)"
+    # Set namespace to 'default' if it is not defined
+    KUBE_TMUX_NAMESPACE="${KUBE_TMUX_NAMESPACE:-default}"
+  fi
+
+  _kube_tmux_get_context
+  _kube_tmux_get_ns
+}
+
+main() {
+  _kube_tmux_update_cache
+
+  local KUBE_TMUX
+
+  # Symbol
+  if [[ "${KUBE_TMUX_SYMBOL_ENABLE}" == true ]]; then
+    KUBE_TMUX+="#[fg=blue]$(_kube_tmux_symbol)#[fg=colour${1}]"
+  fi
+
+  # Context
+  KUBE_TMUX+="#[fg=${2}]${KUBE_TMUX_CONTEXT}"
+
+  # Namespace
+  if [[ "${KUBE_TMUX_NAMESPACE_ENABLE}" == true ]]; then
+    if [[ -n "${KUBE_TMUX_DIVIDER}" ]]; then
+      KUBE_TMUX+="#[fg=colour250]${KUBE_TMUX_DIVIDER}"
+    fi
+    KUBE_TMUX+="#[fg=${3}]${KUBE_TMUX_NAMESPACE}"
+  fi
+
+  echo "${KUBE_TMUX}"
+}
+
+main "$@"
